84dccfd3866e29e6dacd27b4d7c321c01b58e846
Saksham-garg <sakshamgarg142@hotmail.com>
2025-06-30 15:29:53 +0530

    configured pinia store and vue-router

---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
+import ProductListView from "../pages/ProductListView.vue";
+import ProductEditView from "../pages/ProductEditView.vue";
+
+const routes: RouteRecordRaw[] = [
+  { path: "/", component: ProductListView },
+  { path: "/product/:id", component: ProductEditView },
+  { path: "/:catchAll(.*)", redirect: "/" },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
