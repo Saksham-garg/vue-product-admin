bb1d59ced4b8c51534308c82f8e6f2886f383114
Saksham-garg <sakshamgarg142@hotmail.com>
2025-06-30 19:11:18 +0530

    chore: added file path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-// https://vite.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      utils: path.resolve(__dirname, "./src/utils"),
+      router: path.resolve(__dirname, "./src/router"),
+      types: path.resolve(__dirname, "./src/types"),
+      assets: path.resolve(__dirname, "./src/assets"),
+      store: path.resolve(__dirname, "./src/store"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      composables: path.resolve(__dirname, "./src/composables"),
+      components: path.resolve(__dirname, "./src/components"),
+    },
+  },
+});
