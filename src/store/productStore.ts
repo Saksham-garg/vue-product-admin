901f4cb0ca20dac975d460ef8b4123fb5ce2cc2b
Saksham-garg <sakshamgarg142@hotmail.com>
2025-06-30 19:27:30 +0530

    feat: setup store to store & fetch products

---
+++ src/store/productStore.ts
@@ -0,0 +1,47 @@
+import { defineStore } from "pinia";
+import type { Product } from "types/productType";
+
+export const useProductStore = defineStore("product", {
+  state: () => ({
+    products: [] as Product[],
+    loading: false,
+    error: null as string | null,
+  }),
+
+  actions: {
+    async fetchProducts() {
+      this.loading = true;
+      this.error = null;
+      try {
+        const res = await fetch("https://fakestoreapi.com/products");
+        const data = await res.json();
+        console.log(data);
+
+        this.products = data.map((item: any) => ({
+          id: item.id,
+          title: item.title,
+          price: item.price,
+          category: item.category,
+          taxRate: 19, // Default tax rate for new items
+        }));
+        console.log(this.products);
+      } catch (e) {
+        this.error = "Failed to load products";
+        console.error(e);
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    updateProduct(updatedProduct: Product) {
+      const index = this.products.findIndex((p) => p.id === updatedProduct.id);
+      if (index !== -1) {
+        this.products[index] = { ...updatedProduct };
+      }
+    },
+
+    getProductById(id: number): Product | undefined {
+      return this.products.find((p) => p.id === id);
+    },
+  },
+});
